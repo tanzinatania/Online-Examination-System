12277f367780e2cb1d85ef2ab7b3a48323a37db8
Tanzina Nasrin Tania <72453001+tanzinatania@users.noreply.github.com>
2022-03-23 09:30:45 +0600

    Add files via upload

---
+++ online-examination-system/database/project.sql
@@ -0,0 +1,328 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 17, 2021 at 05:21 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `email`, `password`) VALUES
+(1, 'admin@admin.com', 'admin'),
+(2, 'abcd@gmail.com', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answer`
+--
+
+CREATE TABLE `answer` (
+  `qid` text NOT NULL,
+  `ansid` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `answer`
+--
+
+INSERT INTO `answer` (`qid`, `ansid`) VALUES
+('558922117fcef', '5589221195248'),
+('55892211e44d5', '55892211f1fa7'),
+('558922894c453', '558922895ea0a'),
+('558922899ccaa', '55892289aa7cf'),
+('558923538f48d', '558923539a46c'),
+('55892353f05c4', '55892354051be'),
+('6193d94a6c997', '6193d94a8f75d'),
+('6193d94adc95d', '6193d94aeb72a'),
+('6193d94b33096', '6193d94b49f55'),
+('6193d94b63c81', '6193d94b8e4af'),
+('6193d94c546b3', '6193d94c70e7e');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id` text NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `subject` varchar(500) NOT NULL,
+  `feedback` varchar(500) NOT NULL,
+  `date` date NOT NULL,
+  `time` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`id`, `name`, `email`, `subject`, `feedback`, `date`, `time`) VALUES
+('5584ddd0da0ab', 'netcamp', 'sunnygkp10@gmail.com', 'feedback', ';mLBLB', '2015-06-20', '05:28:16am'),
+('558510a8a1234', 'sunnygkp10', 'sunnygkp10@gmail.com', 'dl;dsnklfn', 'fmdsfld fdj', '2015-06-20', '09:05:12am'),
+('5585509097ae2', 'sunny', 'sunnygkp10@gmail.com', 'kcsncsk', 'l.mdsavn', '2015-06-20', '01:37:52pm'),
+('5586ee27af2c9', 'vikas', 'vikas@gmail.com', 'trial feedback', 'triaal feedbak', '2015-06-21', '07:02:31pm'),
+('5589858b6c43b', 'nik', 'nik1@gmail.com', 'good', 'good site', '2015-06-23', '06:12:59pm'),
+('61929830e8837', 'abcd', 'testing@testing.com', 'testing', 'testing', '2021-11-15', '06:26:08pm');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history`
+--
+
+CREATE TABLE `history` (
+  `email` varchar(50) NOT NULL,
+  `eid` text NOT NULL,
+  `score` int(11) NOT NULL,
+  `level` int(11) NOT NULL,
+  `sahi` int(11) NOT NULL,
+  `wrong` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `history`
+--
+
+INSERT INTO `history` (`email`, `eid`, `score`, `level`, `sahi`, `wrong`, `date`) VALUES
+('sunnygkp10@gmail.com', '558921841f1ec', 4, 2, 2, 0, '2015-06-23 09:31:26'),
+('mi5@hollywood.com', '5589222f16b93', 4, 2, 2, 0, '2015-06-23 15:12:56'),
+('nik1@gmail.com', '558921841f1ec', 1, 2, 1, 1, '2015-06-23 16:11:50'),
+('sunnygkp10@gmail.com', '5589222f16b93', 1, 2, 1, 1, '2015-06-24 03:22:38'),
+('abcd@gmail.com', '6193d4513d398', 10, 5, 5, 0, '2021-11-16 16:30:40');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `options`
+--
+
+CREATE TABLE `options` (
+  `qid` varchar(50) NOT NULL,
+  `option` varchar(5000) NOT NULL,
+  `optionid` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `options`
+--
+
+INSERT INTO `options` (`qid`, `option`, `optionid`) VALUES
+('558922117fcef', 'echo', '5589221195248'),
+('558922117fcef', 'print', '558922119525a'),
+('558922117fcef', 'printf', '5589221195265'),
+('558922117fcef', 'cout', '5589221195270'),
+('55892211e44d5', 'int a', '55892211f1f97'),
+('55892211e44d5', '$a', '55892211f1fa7'),
+('55892211e44d5', 'long int a', '55892211f1fb4'),
+('55892211e44d5', 'int a$', '55892211f1fbd'),
+('558922894c453', 'cin>>a;', '558922895ea0a'),
+('558922894c453', 'cin<<a;', '558922895ea26'),
+('558922894c453', 'cout>>a;', '558922895ea34'),
+('558922894c453', 'cout<a;', '558922895ea41'),
+('558922899ccaa', 'cout', '55892289aa7cf'),
+('558922899ccaa', 'cin', '55892289aa7df'),
+('558922899ccaa', 'print', '55892289aa7eb'),
+('558922899ccaa', 'printf', '55892289aa7f5'),
+('558923538f48d', '255.0.0.0', '558923539a46c'),
+('558923538f48d', '255.255.255.0', '558923539a480'),
+('558923538f48d', '255.255.0.0', '558923539a48b'),
+('558923538f48d', 'none of these', '558923539a495'),
+('55892353f05c4', '192.168.1.100', '5589235405192'),
+('55892353f05c4', '172.168.16.2', '55892354051a3'),
+('55892353f05c4', '10.0.0.0.1', '55892354051b4'),
+('55892353f05c4', '11.11.11.11', '55892354051be'),
+('6193d94a6c997', ' Database', '6193d94a8f757'),
+('6193d94a6c997', 'Filters', '6193d94a8f75c'),
+('6193d94a6c997', 'Objects', '6193d94a8f75d'),
+('6193d94a6c997', 'Any of the above', '6193d94a8f75e'),
+('6193d94adc95d', 'Database created using DBMS package', '6193d94aeb72a'),
+('6193d94adc95d', 'Database created in MS-Word', '6193d94aeb731'),
+('6193d94adc95d', 'Database created in EXCEL', '6193d94aeb732'),
+('6193d94adc95d', 'All of the above', '6193d94aeb734'),
+('6193d94b33096', 'DDL', '6193d94b49f4d'),
+('6193d94b33096', 'DML', '6193d94b49f53'),
+('6193d94b33096', 'DCL', '6193d94b49f54'),
+('6193d94b33096', 'TCL', '6193d94b49f55'),
+('6193d94b63c81', 'Database', '6193d94b8e4af'),
+('6193d94b63c81', 'Spreadsheet', '6193d94b8e4b4'),
+('6193d94b63c81', 'Management information system', '6193d94b8e4b5'),
+('6193d94b63c81', 'Text file', '6193d94b8e4b6'),
+('6193d94c546b3', 'It is a collection of data arranged in rows', '6193d94c70e78'),
+('6193d94c546b3', 'It is a collection of data arranged in rows and columns', '6193d94c70e7d'),
+('6193d94c546b3', 'It is a collection of data arranged in columns', '6193d94c70e7e'),
+('6193d94c546b3', 'All of the above', '6193d94c70e7f');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `eid` text NOT NULL,
+  `qid` text NOT NULL,
+  `qns` text NOT NULL,
+  `choice` int(10) NOT NULL,
+  `sn` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`eid`, `qid`, `qns`, `choice`, `sn`) VALUES
+('558921841f1ec', '558922117fcef', 'what is command for print in php??', 4, 1),
+('558921841f1ec', '55892211e44d5', 'which is a variable of php??', 4, 2),
+('5589222f16b93', '558922894c453', 'what is correct statement in c++??', 4, 1),
+('5589222f16b93', '558922899ccaa', 'which command is use for print the output in c++?', 4, 2),
+('558922ec03021', '558923538f48d', 'what is correct mask for A class IP???', 4, 1),
+('558922ec03021', '55892353f05c4', 'which is not a private IP??', 4, 2),
+('6193d4513d398', '6193d94a6c997', 'Unlike filters queries can be saved as in a database.', 4, 1),
+('6193d4513d398', '6193d94adc95d', 'External database is', 4, 2),
+('6193d4513d398', '6193d94b33096', 'ROLLBACK in a database is ____ statement.', 4, 3),
+('6193d4513d398', '6193d94b63c81', 'A collection of interrelated records is called a', 4, 4),
+('6193d4513d398', '6193d94c546b3', 'What is a database?', 4, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz`
+--
+
+CREATE TABLE `quiz` (
+  `eid` text NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `sahi` int(11) NOT NULL,
+  `wrong` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `time` bigint(20) NOT NULL,
+  `intro` text NOT NULL,
+  `tag` varchar(100) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `quiz`
+--
+
+INSERT INTO `quiz` (`eid`, `title`, `sahi`, `wrong`, `total`, `time`, `intro`, `tag`, `date`) VALUES
+('558921841f1ec', 'Php Coding', 2, 1, 2, 5, '', 'PHP', '2015-06-23 09:06:12'),
+('5589222f16b93', 'C++ Coding', 2, 1, 2, 5, '', 'c++', '2015-06-23 09:09:03'),
+('558922ec03021', 'Networking', 2, 1, 2, 5, '', 'networking', '2015-06-23 09:12:12'),
+('6193d4513d398', 'Web Database Programming', 2, 0, 5, 10, 'asdfghrtew\r\n\r\n\r\n\r\n\r\n\r\n', 'abcd', '2021-11-16 15:54:57');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rank`
+--
+
+CREATE TABLE `rank` (
+  `email` varchar(50) NOT NULL,
+  `score` int(11) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `rank`
+--
+
+INSERT INTO `rank` (`email`, `score`, `time`) VALUES
+('sunnygkp10@gmail.com', 9, '2015-06-24 03:22:38'),
+('mi5@hollywood.com', 4, '2015-06-23 15:12:56'),
+('nik1@gmail.com', 1, '2015-06-23 16:11:50'),
+('abcd@gmail.com', 10, '2021-11-16 16:30:40');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `name` varchar(50) NOT NULL,
+  `gender` varchar(5) NOT NULL,
+  `college` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `mob` bigint(20) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`name`, `gender`, `college`, `email`, `mob`, `password`) VALUES
+('Tanzina Nasrin Tania', 'F', 'abcd', 'abcd@gmail.com', 1700000000, '827ccb0eea8a706c4c34a16891f84e7b'),
+('Komal', 'F', 'KNIT sultanpur', 'komalpd2011@gmail.com', 7785068889, 'e10adc3949ba59abbe56e057f20f883e'),
+('Tom Cruze', 'M', 'Hollywood', 'mi5@hollywood.com', 7785068889, 'e10adc3949ba59abbe56e057f20f883e'),
+('Netcamp', 'M', 'KNIT sultanpur', 'netcamp@gmail.com', 987654321, 'e10adc3949ba59abbe56e057f20f883e'),
+('Nikunj', 'M', 'XYZ', 'nik1@gmail.com', 987, '202cb962ac59075b964b07152d234b70'),
+('Sunny', 'M', 'KNIT sultanpur', 'sunnygkp10@gmail.com', 7785068889, 'e10adc3949ba59abbe56e057f20f883e'),
+('User', 'M', 'cimt', 'user@user.com', 11, 'e10adc3949ba59abbe56e057f20f883e'),
+('Vikash', 'M', 'KNIT sultanpur@gmail.com', 'vikash@gmail.com', 7785068889, 'e10adc3949ba59abbe56e057f20f883e');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
